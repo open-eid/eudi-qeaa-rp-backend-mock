b8e3877bb51e79ae354a61ab4833eec59c9fa06e
Mart Aarma <mart.aarma@nortal.com>
2024-01-18 09:46:42 +0200

    MDL-8 Add empty Spring Boot application

---
+++ src/main/java/ee/ria/eudi/qeaa/rp/backend/RpBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.ria.eudi.qeaa.rp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/ria/eudi/qeaa/rp/backend/RpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ria.eudi.qeaa.rp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
